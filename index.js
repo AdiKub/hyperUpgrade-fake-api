63062a5840926cb9834a755c0b19e7ea82d32f0d
adilet <adiletkub@gmail.com>
2019-10-28 09:03:46 +0600

    delete unuseble codes

--- index.js
+++ index.js
@@ -132,30 +132,6 @@ server.post('/auth/login', (req, res) => {
   res.status(200).json({access_token, user})
 })
 
-// server.use(/^(?!\/auth).*$/,  (req, res, next) => {
-//   if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-//     const status = 401
-//     const message = 'Error in authorization format'
-//     res.status(status).json({status, message})
-//     return
-//   }
-//   try {
-//     let verifyTokenResult;
-//      verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
-
-//      if (verifyTokenResult instanceof Error) {
-//        const status = 401
-//        const message = 'Access token not provided'
-//        res.status(status).json({status, message})
-//        return
-//      }
-//      next()
-//   } catch (err) {
-//     const status = 401
-//     const message = 'Error access_token is revoked'
-//     res.status(status).json({status, message})
-//   }
-// })
 
 const router = jsonServer.router(objectDB)
 const middlewares = jsonServer.defaults()
